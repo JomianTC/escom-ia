a8a7a61015141d6c04f7573ade3322b2880bd843
TonyAyala <antonioayalam2001@gmail.com>
2024-04-12 16:03:43 -0600

    server pruebas

--- Frontend/api/routes/teacher.js
+++ Frontend/api/routes/teacher.js
@@ -3,10 +3,8 @@ const router = express.Router();
 const Profesores = require('../data/Profesores.json');
 function checkHeaderToken (req, res, next) {
     const { authorization } = req.headers
-    if (!authorization) {
-        return res.status(403).json({ message: 'No tienes acceso' });
-    }
-    if (authorization.includes('Bearer') && authorization.includes('tony') ) {
+    if (authorization.includes('Bearer') && ( authorization.includes("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IjEwMTAxMDEwMTBAYWx1bW5vLmlwbi5teCIsImlhdCI6MTcxMjc4NjE3OCwiZXhwIjoyMDcyNzgyNTc4fQ.KWQ91qRPjF2PxWXJQ6OJDf0HM2po64T1cF0lhueRYaI")
+    )) {
         console.log("Autorizazado");
         next();
     } else {
@@ -21,32 +19,32 @@ router.get('/renew', function (req, res, next) {
 })
 
 router.get('/', function (req, res, next) {
-    console.log("Estoy en la ruta de profesores");
-    const { limit = 10, page=1 } = req.query
+    const { limit=10, page=1 } = req.query
 
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
     
     res.status(200).json({
         data:{
-            teachers: Profesores.slice(startIndex, endIndex),
+            profesores: Profesores.slice(startIndex, endIndex),
             total: Profesores.length
         }
     });
 });
-router.get('/:id', function (req, res, next) {
+router.get('/:id', checkHeaderToken, function (req, res, next) {
     const { id } = req.params;
     const game = Profesores.find(profesor => profesor.id === id);
     if (!game) {
         return res.status(404).json({ message: 'No se encontro el personaje' });
     }
-    res.status(200).json({ teacherFound: game });
+    res.status(200).json({ ...game });
 })
 
 router.post('/', async function (req, res, next) {
     // Simulación tiempo de respuesta
-    await new Promise((r) => setTimeout(r, 6000))
+    // await new Promise((r) => setTimeout(r, 6000))
     const { body } = req;
+    console.log(body);
     const { nombre, area, grado_academico, email, contacto, foto_perfil } = body;
     const newProfesor = {
         id: Math.random().toString(16).slice(2),
@@ -57,17 +55,18 @@ router.post('/', async function (req, res, next) {
         contacto,
         foto_perfil
     }
-    Profesores.push(newProfesor);
+    // Profesores.push(newProfesor);
     console.log("Se ha creado un nuevo profesor", newProfesor);
-    res.status(201).json({ message: "Creado",teacher: newProfesor });
+    res.status(201).json({ message: "Creado",profesor: newProfesor });
 });
 router.delete('/:id', function (req, res, next) { 
     const { id } = req.params;
-    const index = Profesores.findIndex(profesor => profesor.id === id);
-    if (index === -1) {
+    console.log("Profesor eliminado", id);
+    const profesor = Profesores.find(profesor => profesor.id === id);
+    console.log(profesor);
+    if (!profesor) {
         return res.status(404).json({ message: 'No se encontro el profesor' });
     }
-    Profesores.splice(index, 1);
     res.status(200).json({ message: 'Profesor eliminado' });
 });
 
--- Frontend/api/routes/user.js
+++ Frontend/api/routes/user.js
@@ -20,82 +20,99 @@ const router = express.Router();
 
 router.get("/", function (req, res, next) {
     res.status(200).json({ title: "Bienvenido" });
-})
-// Aqui ya estaríamos creando una ruta para el usuario que quiere registrarse por ejemplo
-router.post("/register", function (req, res, next) {
+  })
+  // Aqui ya estaríamos creando una ruta para el usuario que quiere registrarse por ejemplo
+  router.post("/register", function (req, res, next) {
     console.log(req.body);
-    const data = {
-        token: "lfkgjldfjglkdfjglkdfjglkjdflkgjlkdfj",
-        nombres: "Tony",
-        boleta: "2020630319",
-        foto_perfil: "No hay",
-        apellidos: "Mora",
-        contrasena: "@200120Tm",
-        email_academico: "antonioayalam2001@gmail.com",
-        email_recuperacion: "antonioayalam2001@gmail.com",
-        programa_academico: "ISC-2009",
-    }
+
     return res
-        .status(200)
-        .json({
-            message: "Exito",
-            user: data
-        });
-})
-router.post("/login", function (req, res, next) {
+      .status(201)
+      .json({
+        "mensaje": "Usuario registrado con exito!",
+        "usuario": {
+            "nombres": "Jose Antonio",
+            "apellidos": "Mora Ayala",
+            "boleta": "2020630123",
+            "email_academico": "jmoraa12@alumno.ipn.mx",
+            "email_recuperacion": "tonyayala12@gmail.com",
+            "programa_academico": "ISC-2009",
+            "id": "a0df838d-559f-4fb1-8296-c3f0d7c2d81f",
+            "foto_perfil": ""
+        }
+    });
+  })
+  router.post("/login", function (req, res, next) {
     console.log(req.body);
     const data = {
-        nombres: "Tony",
-        boleta: "2020630319",
-        foto_perfil: "",
-        apellidos: "Mora",
-        contrasena: "@200120Tm",
-        email_academico: "antonioayalam2001@gmail.com",
-        email_recuperacion: "antonioayalam2001@gmail.com",
-        programa_academico: "ISC-2009",
+      nombres: "Tony",
+      boleta: "2020630319",
+      foto_perfil: "",
+      apellidos: "Mora",
+      contrasena: "@200120Tm",
+      email_academico: "antonioayalam2001@gmail.com",
+      email_recuperacion: "antonioayalam2001@gmail.com",
+      programa_academico: "ISC-2009",
+    }
+    if (req.body.boleta !== "2020630319" || req.body.contrasena !== "@200120Tm") {
+      return res.status(401).json({ message: "Credenciales incorrectas" });
     }
     return res.status(200).json({
-        message: "Logeado",
-        user: data,
-        token: "tony",
-    });
-})
-router.post("/login/admin", function (req, res, next) {
+      "mensaje": "Inicio de sesión exitoso!",
+      "usuario": {
+          "id": "c0d56f1d-835e-45ab-bdfc-fc1b2a89c62c",
+          "nombres": "qweqwe",
+          "apellidos": "asdasd",
+          "boleta": "1010101010",
+          "email_academico": "1010101010@alumno.ipn.mx",
+          "email_recuperacion": "1010101010@gmail.com",
+          "programa_academico": "ISC09",
+          "foto_perfil": ""
+      },
+      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IjEwMTAxMDEwMTBAYWx1bW5vLmlwbi5teCIsImlhdCI6MTcxMjc4NjE3OCwiZXhwIjoyMDcyNzgyNTc4fQ.KWQ91qRPjF2PxWXJQ6OJDf0HM2po64T1cF0lhueRYaI"
+  });
+  })
+  router.post("/login/admin", function (req, res, next) {
 
     const data = {
-        identificador: "Tunas",
-        nombre: "Tunas",
-        email: "Tunas",
-        area: "Tunas",
+      identificador: "Tunas",
+      nombre: "Tunas",
+      email: "Tunas",
+      area: "Tunas",
     }
     return res.status(200).json({
-        message: "Admin",
-        user: data,
-        token: "tony",
-    });
-})
+      "mensaje": "Inicio de sesión exitoso!",
+      "admin": {
+          "id": "61b0f918-d868-44a9-bfcb-65dedbb422d4",
+          "nombre": "Super SU Admin",
+          "email": "supersuadmin@ipn.mx",
+          "area": "Servicios SU",
+          "foto_perfil": ""
+      },
+      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InN1cGVyU3VBZG1pbkBpcG4ubXgiLCJpYXQiOjE3MTI3ODYxOTAsImV4cCI6MjA3Mjc4MjU5MH0.GALLeTMsFQQcLDozEC4l0P-kmB30xzx43uuzxwZbNxM"
+  });
+  })
 
-router.post("/register/admin", function (req, res, next) {
+  router.post("/register/admin", function (req, res, next) {
 
     const data = {
-        identificador: "Tunas",
-        nombre: "Tunas",
-        email: "Tunas",
-        area: "Tunas",
+      identificador: "Tunas",
+      nombre: "Tunas",
+      email: "Tunas",
+      area: "Tunas",
     }
-    return res.status(200).json({
-        message: "Admin registrado",
-        user: data,
-    });
-})
+    return res.status(200).json({ 
+      message: "Admin registrado",
+      user: data,
+     });
+  })
 
-router.get("/token", function (req, res, next) {
+  router.get("/token", function (req, res, next) {
     return res.status(200).json({ title: "Token" });
-})
+  })
 
-router.get('/renew', function (req, res, next) {
-    console.log("Estoy renovando el token");
-    res.status(200).json({ token: 'tony' });
+  router.get('/check-auth', function (req, res, next) {
+  console.log("Estoy renovando el token");
+  res.status(200).json({ token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IjEwMTAxMDEwMTBAYWx1bW5vLmlwbi5teCIsImlhdCI6MTcxMjc4NjE3OCwiZXhwIjoyMDcyNzgyNTc4fQ.KWQ91qRPjF2PxWXJQ6OJDf0HM2po64T1cF0lhueRYaI' });
 })
 
 module.exports = router;

6fdb60fd8e60155b139fb121609fafbbb15b36af
JomianTC <jomiantoca-2011@hotmail.com>
2024-04-25 16:23:59 -0600

    Backend - Cambio de BD a Postgress

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -33,7 +33,7 @@ import { MailerModule } from "@nestjs-modules/mailer";
 		// ? Importar el módulo de TypeORM para la BD
 		TypeOrmModule.forRoot({
 
-			type: "mysql",
+			type: "postgres",
 
 			host: process.env.DB_HOST,
 			port: +process.env.DB_PORT,
